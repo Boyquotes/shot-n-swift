2bb309b7ef994945f491fdb684fb081f8b3e21d0
David Osaro <90871488+davidosaro@users.noreply.github.com>
2023-06-07 22:11:36 +0100

    added logo

--- scenes/splash/Splash.gd
+++ scenes/splash/Splash.gd
@@ -4,11 +4,21 @@ onready var main_scene = load("res://scenes/mainmenu/MainMenu.tscn")
 onready var world_scene = load("res://scenes/world/World.tscn")
 onready var timer = $Timer
 onready var anim = $AnimationPlayer
+onready var tween = $Tween
 
 func _ready():
 	anim.play("fade_in")
-	$Tween.start()
-	$Tween.interpolate_property($body/TextureRect,"rect_scale",Vector2(0.5,0.5),Vector2(1,1),0.3,Tween.TRANS_LINEAR,Tween.EASE_IN)
+	tween.start()
+	tween.interpolate_property($body/TextureRect,"rect_scale",Vector2(1,1),Vector2(1,1),0.4,Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+	
+	yield(tween,"tween_completed")
+	timer.start()
+	
+	yield(timer, "timeout")
+	anim.play("fade_out")
+#	tween.start()
+#	tween.interpolate_property($body/TextureRect,"rect_scale",Vector2(1.1,1.1),Vector2(1,3),0.4,Tween.TRANS_BOUNCE, Tween.EASE_IN)
+	
 #INSTANCE NEXT SCENE 
 func _next():
 	var menu = main_scene.instance()
